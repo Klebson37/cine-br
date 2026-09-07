df7fcb48ea478dc10c534e949ce099ebf0156249
Klebson Gomes <kgdsklebson@gmail.com>
2026-09-07 00:49:15 -0300

    feat: adiciona as rotas das listas marcadas

---
+++ app/quero-assistir/loading.tsx
@@ -0,0 +1,13 @@
+import { GridSkeleton } from '@/components/catalog/GridSkeleton'
+import { ListTabs } from '@/components/layout/ListTabs'
+
+export default function Loading() {
+  return (
+    <div className="wrap pt-10">
+      <ListTabs active="want" />
+      <div className="mt-10">
+        <GridSkeleton />
+      </div>
+    </div>
+  )
+}
